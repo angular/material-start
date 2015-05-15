7441a142913721009a8997e545652a3446149e24
Marcy Sutton <marcy@substantial.com>
2015-05-15 13:14:17 -0700

    fix: add working e2e tests
    Closes #30

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -4,38 +4,25 @@
 
 describe('my app', function() {
 
+  var listView = require('./support/list-view.js');
+  var bottomSheetView = require('./support/bottom-sheet-view.js');
+
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should load a list of users', function() {
+    expect(listView.listItems.count()).toBeGreaterThan(1);
   });
 
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
+  describe('selecting a user', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view2');
+      element(by.css('button.share')).click();
     });
 
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+    it('should handle focus on view change', function() {
+      bottomSheetView.actionButtons.then(function(items) {
+        expect(items[0].getAttribute('id')).toEqual(browser.driver.switchTo().activeElement().getAttribute('id'));
+      });
     });
 
   });
---
+++ e2e-tests/support/list-view.js
@@ -0,0 +1,6 @@
+var ListView = function() {
+  this.listItems = element.all(by.css('md-list-item'));
+};
+
+module.exports = new ListView();
+
