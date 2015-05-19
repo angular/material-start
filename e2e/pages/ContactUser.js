ea5154486d8bd7a51e3178a9aca3c9d9d970a2f3
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-05-18 19:48:06 -0500

    update(e2e): add sleep() to resolve $animate bugs

--- e2e/pages/ContactUser.js
+++ e2e/pages/ContactUser.js
@@ -5,6 +5,9 @@ var ContactUser = function() {
   };
 
   this.buttons = function() {
+    // @Todo - remove sleep() when https://github.com/angular/protractor/issues/2154
+    browser.sleep(500);
+
     return element.all(by.css('[ng-click="cp.submitContact(item)"]'));
   };
 
