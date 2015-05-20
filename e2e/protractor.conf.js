ad688ce42eb01848e3df668554f3621666165749
Marcy Sutton <marcy@substantial.com>
2015-05-20 11:49:34 -0700

    Cleaning up config

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -11,17 +11,14 @@ exports.config = {
 
   baseUrl: 'http://localhost:8080',
 
-
-  chromeDriver: '/usr/local/lib/node_modules/protractor/selenium/chromedriver',
-  seleniumServerJar: '/usr/local/lib/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
-  //seleniumAddress:'http://127.0.0.1:4444/wd/hub',
+  seleniumAddress:'http://127.0.0.1:4444/wd/hub',
 
   framework: 'jasmine',
 
-  //plugins: [{
-  //  chromeA11YDevTools: true,
-  //  path: '../node_modules/protractor/plugins/accessibility/index.js'
-  //}],
+  plugins: [{
+   chromeA11YDevTools: true,
+   path: '../node_modules/protractor/plugins/accessibility/index.js'
+  }],
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
