865ef17d3ec1d122a8c0a76384459c28537f42b1
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-09-25 07:15:16 -0500

    update(build): upgrade to required ng 1.4.6

--- app/config.js
+++ app/config.js
@@ -13,26 +13,26 @@ System.config({
 
 System.config({
   "map": {
-    "angular": "github:angular/bower-angular@1.4.1",
-    "angular-animate": "github:angular/bower-angular-animate@1.4.1",
-    "angular-aria": "github:angular/bower-angular-aria@1.4.1",
+    "angular": "github:angular/bower-angular@1.4.6",
+    "angular-animate": "github:angular/bower-angular-animate@1.4.6",
+    "angular-aria": "github:angular/bower-angular-aria@1.4.6",
     "angular-material": "github:angular/bower-material@master",
-    "css": "github:systemjs/plugin-css@0.1.13",
+    "css": "github:systemjs/plugin-css@0.1.18",
     "json": "github:systemjs/plugin-json@0.1.0",
     "text": "github:systemjs/plugin-text@0.0.2",
     "traceur": "github:jmcriffey/bower-traceur@0.0.90",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.90",
-    "github:angular/bower-angular-animate@1.4.1": {
-      "angular": "github:angular/bower-angular@1.4.1"
+    "github:angular/bower-angular-animate@1.4.6": {
+      "angular": "github:angular/bower-angular@1.4.6"
     },
-    "github:angular/bower-angular-aria@1.4.1": {
-      "angular": "github:angular/bower-angular@1.4.1"
+    "github:angular/bower-angular-aria@1.4.6": {
+      "angular": "github:angular/bower-angular@1.4.6"
     },
     "github:angular/bower-material@master": {
-      "angular": "github:angular/bower-angular@1.4.1",
-      "angular-animate": "github:angular/bower-angular-animate@1.4.1",
-      "angular-aria": "github:angular/bower-angular-aria@1.4.1",
-      "css": "github:systemjs/plugin-css@0.1.13"
+      "angular": "github:angular/bower-angular@1.4.6",
+      "angular-animate": "github:angular/bower-angular-animate@1.4.6",
+      "angular-aria": "github:angular/bower-angular-aria@1.4.6",
+      "css": "github:systemjs/plugin-css@0.1.18"
     }
   }
 });
