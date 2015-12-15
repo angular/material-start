cbe966354b29e35a6a0eb0653ea5a072bc1e3301
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-12-14 23:19:59 -0600

    Update config.js

--- app/config.js
+++ app/config.js
@@ -11,25 +11,26 @@ System.config({
   },
 
   map: {
-    "angular": "github:angular/bower-angular@1.4.7",
-    "angular-animate": "github:angular/bower-angular-animate@1.4.7",
-    "angular-aria": "github:angular/bower-angular-aria@1.4.7",
+    "angular": "github:angular/bower-angular@1.4.8",
+    "angular-animate": "github:angular/bower-angular-animate@1.4.8",
+    "angular-aria": "github:angular/bower-angular-aria@1.4.8",
+    "angular-messages": "github:angular/bower-angular-messages@1.4.8",
     "angular-material": "github:angular/bower-material@master",
     "css": "github:systemjs/plugin-css@0.1.18",
     "json": "github:systemjs/plugin-json@0.1.0",
     "text": "github:systemjs/plugin-text@0.0.2",
     "traceur": "github:jmcriffey/bower-traceur@0.0.91",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
-    "github:angular/bower-angular-animate@1.4.7": {
-      "angular": "github:angular/bower-angular@1.4.7"
+    "github:angular/bower-angular-animate@1.4.8": {
+      "angular": "github:angular/bower-angular@1.4.8"
     },
-    "github:angular/bower-angular-aria@1.4.7": {
-      "angular": "github:angular/bower-angular@1.4.7"
+    "github:angular/bower-angular-aria@1.4.8": {
+      "angular": "github:angular/bower-angular@1.4.8"
     },
     "github:angular/bower-material@master": {
-      "angular": "github:angular/bower-angular@1.4.7",
-      "angular-animate": "github:angular/bower-angular-animate@1.4.7",
-      "angular-aria": "github:angular/bower-angular-aria@1.4.7",
+      "angular": "github:angular/bower-angular@1.4.8",
+      "angular-animate": "github:angular/bower-angular-animate@1.4.8",
+      "angular-aria": "github:angular/bower-angular-aria@1.4.8",
       "css": "github:systemjs/plugin-css@0.1.18"
     }
   }
