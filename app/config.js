0a67251efc85baa861ef186191892606c4757f39
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-02-17 15:31:13 +0100

    update(libs): update to v 1.4.9

--- app/config.js
+++ app/config.js
@@ -11,26 +11,26 @@ System.config({
   },
 
   map: {
-    "angular": "github:angular/bower-angular@1.4.8",
-    "angular-animate": "github:angular/bower-angular-animate@1.4.8",
-    "angular-aria": "github:angular/bower-angular-aria@1.4.8",
-    "angular-messages": "github:angular/bower-angular-messages@1.4.8",
+    "angular": "github:angular/bower-angular@1.4.9",
+    "angular-animate": "github:angular/bower-angular-animate@1.4.9",
+    "angular-aria": "github:angular/bower-angular-aria@1.4.9",
     "angular-material": "github:angular/bower-material@master",
+    "angular-messages": "github:angular/bower-angular-messages@1.4.9",
     "css": "github:systemjs/plugin-css@0.1.19",
     "json": "github:systemjs/plugin-json@0.1.0",
     "text": "github:systemjs/plugin-text@0.0.2",
     "traceur": "github:jmcriffey/bower-traceur@0.0.91",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
-    "github:angular/bower-angular-animate@1.4.8": {
-      "angular": "github:angular/bower-angular@1.4.8"
+    "github:angular/bower-angular-animate@1.4.9": {
+      "angular": "github:angular/bower-angular@1.4.9"
     },
-    "github:angular/bower-angular-aria@1.4.8": {
-      "angular": "github:angular/bower-angular@1.4.8"
+    "github:angular/bower-angular-aria@1.4.9": {
+      "angular": "github:angular/bower-angular@1.4.9"
     },
     "github:angular/bower-material@master": {
-      "angular": "github:angular/bower-angular@1.4.8",
-      "angular-animate": "github:angular/bower-angular-animate@1.4.8",
-      "angular-aria": "github:angular/bower-angular-aria@1.4.8",
+      "angular": "github:angular/bower-angular@1.4.9",
+      "angular-animate": "github:angular/bower-angular-animate@1.4.9",
+      "angular-aria": "github:angular/bower-angular-aria@1.4.9",
       "css": "github:systemjs/plugin-css@0.1.18"
     }
   }
