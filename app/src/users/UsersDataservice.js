0d842da68ee7a5697e6227dba4bd6d39bb5c2e27
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-04-20 09:00:44 -0500

    fix(icons): reuse existing avatar icons

--- app/src/users/UsersDataservice.js
+++ app/src/users/UsersDataservice.js
@@ -30,12 +30,12 @@ function UsersDataservice($q, $log) {
         },
         {
           name: 'Ernesto Urbina',
-          avatar: 'svg-5',
+          avatar: 'svg-2',
           content: 'Webtwo ipsum dolor sit amet, eskobo chumby doostang bebo. Bubbli greplin stypi prezi mzinga heroku wakoopa, shopify airbnb dogster dopplr gooru jumo, reddit plickers edmodo stypi zillow etsy.'
         },
         {
           name: 'Gani Ferrer',
-          avatar: 'svg-6',
+          avatar: 'svg-1',
           content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
         }
       ];
