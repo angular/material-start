e52f112e84260c235d58bccf210f990087a298d0
Topher Fangio <fangiotophia@gmail.com>
2016-08-08 14:30:56 -0500

    ES6 Tutorial - Step 6

--- app/src/app.js
+++ app/src/app.js
@@ -9,7 +9,11 @@ import AppController from 'src/AppController';
 import Users from 'src/users/Users';
 
 export default angular.module( 'starter-app', [ 'ngMaterial', Users.name ] )
-  .run(() => {
-    console.log(`Starting the 'starter-app' module`);
+  .config(($mdIconProvider) => {
+    // Register the user `avatar` icons
+    $mdIconProvider
+      .defaultIconSet("./assets/svg/avatars.svg", 128)
+      .icon("menu", "./assets/svg/menu.svg", 24)
+      .icon("share", "./assets/svg/share.svg", 24);
   })
   .controller('AppController', AppController);
