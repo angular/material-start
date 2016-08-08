313d8a5511dbf61a53923d2876252135b2e932da
Topher Fangio <fangiotophia@gmail.com>
2016-08-08 14:50:29 -0500

    ES6 Tutorial - Step 10

--- app/src/users/Users.js
+++ app/src/users/Users.js
@@ -2,9 +2,15 @@
 
 import UsersDataService from 'src/users/services/UsersDataService';
 
+import UsersList from 'src/users/components/list/UsersList';
+import UserDetails from 'src/users/components/details/UserDetails';
+
 // Define the Angular 'users' module
 
 export default angular
   .module("users", ['ngMaterial'])
+
+  .directive("usersList", () => new UsersList)
+  .directive("userDetails", () => new UserDetails)
 
   .service("UsersDataService", UsersDataService);
