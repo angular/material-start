85df7a2ff0e5d6dc2cc23ac1d4d2678f18986ad7
Topher Fangio <fangiotophia@gmail.com>
2016-08-08 14:32:52 -0500

    ES6 Tutorial - Step 7

--- app/src/app.js
+++ app/src/app.js
@@ -14,6 +14,10 @@ export default angular.module( 'starter-app', [ 'ngMaterial', Users.name ] )
     $mdIconProvider
       .defaultIconSet("./assets/svg/avatars.svg", 128)
       .icon("menu", "./assets/svg/menu.svg", 24)
-      .icon("share", "./assets/svg/share.svg", 24);
+      .icon("share", "./assets/svg/share.svg", 24)
+      .icon("google_plus", "./assets/svg/google_plus.svg", 24)
+      .icon("hangouts", "./assets/svg/hangouts.svg", 24)
+      .icon("twitter", "./assets/svg/twitter.svg", 24)
+      .icon("phone", "./assets/svg/phone.svg", 24);
   })
   .controller('AppController', AppController);
